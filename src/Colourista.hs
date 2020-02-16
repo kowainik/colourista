07bd99322047a7b65bf13a45194c69fa2dd81e18
vrom911 <vrom911@gmail.com>
2020-02-16 13:43:52 +0000

    [#2] Bring pure interface functions
    Resolves #2

--- src/Colourista.hs
+++ src/Colourista.hs
@@ -1,6 +1,19 @@
+{- |
+Copyright: (c) 2020 Kowainik
+SPDX-License-Identifier: MPL-2.0
+Maintainer: Kowainik <xrom.xkov@gmail.com>
+
+Improve your terminal output with the handy helpers of @ansi-terminal@ functions
+that 'Colourista' provides to you.
+-}
+
 module Colourista
-       ( someFunc
+       ( -- $pure
+         module Colourista.Pure
        ) where
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+{- $pure
+The set of pure functions that represents codes to use for the terminal output
+customisation: colouring and emphasis.
+-}
+import Colourista.Pure
