eddaca75a1e8808d69d5261b819053cd727b150d
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-08-25 17:31:10 +0100

    [#41] Add some tests
    Resolves #41

---
+++ test/Test/Colourista.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE ImplicitParams #-}
+
+module Test.Colourista
+    ( colouristaSpec
+    ) where
+
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+import Colourista (ColourMode (..), HasColourMode, formatWith, italic, red, reset, yellow)
+import Colourista.Short (b, i, u)
+
+
+colouristaSpec :: Spec
+colouristaSpec = describe "Colourista tests" $ do
+    describe "Colour codes are actual strings" $ do
+        it "Yellow: String" $
+            yellow @String `shouldBe` "\ESC[93m"
+        it "Yellow: Text" $
+            yellow @Text `shouldBe` "\ESC[93m"
+        it "Yellow: ByteString" $
+            yellow @ByteString `shouldBe` "\ESC[93m"
+        it "Reset: Text" $
+            reset @Text `shouldBe` "\ESC[0m"
+
+    describe "Colourista.Short" $ do
+        it "Bold" $
+            b @Text "bold" `shouldBe` "\ESC[1mbold\ESC[0m"
+        it "Italic" $
+            i @Text "italic" `shouldBe` "\ESC[3mitalic\ESC[0m"
+        it "Underline" $
+            u @Text "underline" `shouldBe` "\ESC[4munderline\ESC[0m"
+
+    describe "'formatWith' works" $ do
+        it "Format with empty list" $
+            formatWith @Text [] "text" `shouldBe` "text"
+        it "Format with red italic" $
+            formatWith @Text [red, italic] "text" `shouldBe` "\ESC[91m\ESC[3mtext\ESC[0m"
+
+    describe "Colour disabling works" $ do
+        it "Disable yellow" $
+            let ?colourMode = DisableColour
+            in yellow @Text `shouldBe` ""
+        it "Disable formatWith" $
+            let ?colourMode = DisableColour
+            in formatWith @Text [red, italic] "plain" `shouldBe` "plain"
+        it "Disable short bold" $
+            let ?colourMode = DisableColour
+            in b @Text "thin" `shouldBe` "thin"
+        it "DisableColour propagates to custom functions" $
+            let ?colourMode = DisableColour
+            in customPainting `shouldBe` "not formatted"
+        it "DisableColour doesn't propagate to custom functions without constraint" $
+            let ?colourMode = DisableColour
+            in colourWithIgnore `shouldBe` "\ESC[93m\ESC[3mpretty\ESC[0m"
+
+customPainting :: HasColourMode => Text
+customPainting = formatWith [yellow, italic] "not formatted"
+
+colourWithIgnore :: Text
+colourWithIgnore = formatWith [yellow, italic] "pretty"
